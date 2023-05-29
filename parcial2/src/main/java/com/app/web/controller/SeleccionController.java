f2d313a62dcad33beeae4e8410ef3d16247c3e44
MaicolRuiz01 <maicolandreyruga@ufps.edu.co>
2023-05-29 12:22:48 -0500

    cambio sellecion conrtoller

--- parcial2/src/main/java/com/app/web/controller/SeleccionController.java
+++ parcial2/src/main/java/com/app/web/controller/SeleccionController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -78,6 +79,13 @@ public class SeleccionController {
 	public List<Seleccion> seleccionByGrupo(@PathVariable String grupo){
 		
 		return seleccionRepo.findByGrupo(grupo);
+	}
+	
+	@GetMapping
+	public String lista(Model model) {
+	    List<Seleccion> selecciones = seleccionRepo.findAll();
+	    model.addAttribute("selecciones", selecciones);
+	    return "index";
 	}
 	
 
